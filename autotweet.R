e83b3cb24fad689628bd22205bbdc864527f85db
ColinFay <contact@colinfay.me>
2017-05-31 08:23:10 +0200

    maj

--- autotweet.R
+++ autotweet.R
@@ -2,13 +2,13 @@ library(magrittr)
 library(dplyr)
 library(rtweet)
 setwd("/home/colin/Bureau/twitter_corpus")
-twitter_corpus <- read.csv("twitter_corpus.csv", stringsAsFactors = FALSE)
+load(file = "twitter_corpus.rda")
 trend <- get_trends() %>%
   arrange(desc(tweet_volume))
-trend <- trend[1,1]
-search <- search_tweets(trend, n = 1000)
-search$trend <- trend
+search <- search_tweets(trend[1,1], n = 500)
+search$trend <- trend[1,1]
 search$created_at <- lubridate::ymd_hms(search$created_at)
 twitter_corpus <- rbind(twitter_corpus, search) %>% 
   unique()
+save(twitter_corpus, file = "twitter_corpus.rda")
 write.csv(x = twitter_corpus, file = "twitter_corpus.csv", row.names = FALSE)
